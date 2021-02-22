ade8cb2a3a4aa66fe24ddc6d9ae48421b45f2c0d
chens_hi <chens_hi@icloud.com>
2021-02-22 12:13:52 +0800

    工具箱: 选中相关资源

---
+++ Assets/GenericKit/Editor/SelectionResources.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using GenericKit.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace GenericKit.Editor
+{
+	/// <summary>
+	/// 选中相关资源
+	/// </summary>
+	public static class SelectionResources
+	{
+		/// <summary>
+		/// 拷贝路径
+		/// </summary>
+		[MenuItem(GenericKitDefine.MENU_PATH_SELECTION_COPY_PATH, false, GenericKitDefine.MENU_PRIORITY_SELECTION_COPY_PATH)]
+		private static void CopyPath()
+		{
+			var sb = new StringBuilder();
+			foreach (var target in Selection.objects)
+			{
+				sb.AppendLine(AssetDatabase.GetAssetPath(target));
+			}
+			GUIUtility.systemCopyBuffer = sb.ToString();
+		}
+
+		/// <summary>
+		/// 打印路径
+		/// </summary>
+		[MenuItem(GenericKitDefine.MENU_PATH_SELECTION_DEBUG_PATH, false, GenericKitDefine.MENU_PRIORITY_SELECTION_DEBUG_PATH)]
+		private static void DebugPath()
+		{
+			foreach (var target in Selection.objects)
+			{
+				Debug.Log(AssetDatabase.GetAssetPath(target), target);
+			}
+		}
+
+		/// <summary>
+		/// 验证选择
+		/// </summary>
+		/// <returns>状态</returns>
+		[MenuItem(GenericKitDefine.MENU_PATH_SELECTION_COPY_PATH, true)]
+		[MenuItem(GenericKitDefine.MENU_PATH_SELECTION_DEBUG_PATH, true)]
+		private static bool VerifySelection()
+		{
+			return Selection.objects.Length > 0;
+		}
+	}
+}
--- Assets/GenericKit/Runtime/GenericKitDefine.cs
+++ Assets/GenericKit/Runtime/GenericKitDefine.cs
@@ -8,9 +8,14 @@
 		private const string _MENU_PATH = "Assets/Tool Kit/";
 		private const int _MENU_PRIORITY = 9000;
 
+		public const string MENU_PATH_SELECTION_COPY_PATH = _MENU_PATH + "Selection/Copy Path";
+		public const int MENU_PRIORITY_SELECTION_COPY_PATH = _MENU_PRIORITY + 101;
+		public const string MENU_PATH_SELECTION_DEBUG_PATH = _MENU_PATH + "Selection/Debug Path";
+		public const int MENU_PRIORITY_SELECTION_DEBUG_PATH = _MENU_PRIORITY + 102;
+		
 		public const string MENU_PATH_QUICKLY_FIND_MATCH = _MENU_PATH + "Find References/Quickly Find Match";
-		public const int MENU_PRIORITY_QUICKLY_FIND_MATCH = _MENU_PRIORITY + 101;
+		public const int MENU_PRIORITY_QUICKLY_FIND_MATCH = _MENU_PRIORITY + 111;
 		public const string MENU_PATH_PRECISE_FIND_MATCH = _MENU_PATH + "Find References/Precise Find Match";
-		public const int MENU_PRIORITY_PRECISE_FIND_MATCH = _MENU_PRIORITY + 102;
+		public const int MENU_PRIORITY_PRECISE_FIND_MATCH = _MENU_PRIORITY + 112;
 	}
 }
